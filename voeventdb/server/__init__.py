6240881ed7819d099f7ccbee84fb546713b0e78f
David Palmer <palmer@lanl.gov>
2024-02-01 20:43:32 -0700

    UPdate versioneer to support python 3.12

--- voeventdb/server/__init__.py
+++ voeventdb/server/__init__.py
@@ -2,3 +2,6 @@ from ._version import get_versions
 __versiondict__ = get_versions()
 __version__ = __versiondict__['version']
 del get_versions
+
+from . import _version
+__version__ = _version.get_versions()['version']
